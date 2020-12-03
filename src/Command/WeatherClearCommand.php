b0419eb17ade651bac6eeb2724ce61508799714f
Thomas Berends <contact@thomasberends.nl>
2020-12-03 10:03:41 +0100

    Update with more commands

---
+++ src/Command/WeatherClearCommand.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Command;
+
+use League\Flysystem\FilesystemInterface;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+
+class WeatherClearCommand extends Command
+{
+    /**
+     * @var FilesystemInterface
+     */
+    private $defaultStorage;
+
+    public function __construct(FilesystemInterface $defaultStorage)
+    {
+        parent::__construct();
+        $this->defaultStorage = $defaultStorage;
+    }
+
+    protected static $defaultName = 'weather:clear';
+
+    protected function configure()
+    {
+        $this
+            ->setDescription('Add a short description for your command')
+        ;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $structure = "CgAACQAEc2l6ZQMAAAADAAAAAQAAAAEAAAABCQAIZW50aXRpZXMAAAAAAAkABmJsb2NrcwoAAAABCQADcG9zAwAAAAMAAAAAAAAAAAAAAAADAAVzdGF0ZQAAAAAACQAHcGFsZXR0ZQoAAAABCAAETmFtZQANbWluZWNyYWZ0OmFpcgADAAtEYXRhVmVyc2lvbgAAB7gA";
+
+        $io = new SymfonyStyle($input, $output);
+
+        $nbtService = new \Nbt\Service(new \Nbt\DataHandler());
+        $structure = $nbtService->readString(base64_decode($structure));
+
+        $palette = $structure->findChildByName('palette');
+        $paletteChildren = $palette->getChildren();
+        $paletteChildren[0]->getChildren()[0]->setValue('minecraft:redstone_block');
+
+        $this->defaultStorage->update('weather_clear.nbt', gzencode($nbtService->writeString($structure)));
+
+        $paletteChildren[0]->getChildren()[0]->setValue('minecraft:black_concrete');
+
+        $this->defaultStorage->update('weather_rain.nbt', gzencode($nbtService->writeString($structure)));
+
+        $io->success('Cleared the weather');
+
+        return 0;
+    }
+}
